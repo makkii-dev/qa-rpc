909a98cda8446dfa444fb2c27042cefa9899addc
MiaoShi09 <miao@aion.network>
2019-02-26 17:25:34 +0800

    use the shortst test case for ci image testing

--- ci_test.sh
+++ ci_test.sh
@@ -7,9 +7,9 @@ nohup /run/aionminer -l 127.0.0.1:8008 -t 1 &
 
 #./node_modules/mocha/bin/mocha testApp.js --type http --testsuite test_cases/TXTC.tsv --no-timeouts --reporter mocha-junit-reporter --reporter-options mochaFile=testReport/TXTC-$(date +%y%m%d%H%M).xml
 
-#./node_modules/mocha/bin/mocha testApp.js --type http --testsuite test_cases/FTTC.tsv --no-timeouts --reporter mocha-junit-reporter --reporter-options mochaFile=testReport/FTTC-$(date +%y%m%d%H%M).xml
+./node_modules/mocha/bin/mocha testApp.js --type http --testsuite test_cases/FTTC.tsv --no-timeouts --reporter mocha-junit-reporter --reporter-options mochaFile=testReport/FTTC-$(date +%y%m%d%H%M).xml
 
-./node_modules/mocha/bin/mocha testApp.js --type http --testsuite test_cases/bugs.tsv --no-timeouts --reporter mocha-junit-reporter --reporter-options mochaFile=testReport/bugs-$(date +%y%m%d%H%M).xml
+#./node_modules/mocha/bin/mocha testApp.js --type http --testsuite test_cases/bugs.tsv --no-timeouts --reporter mocha-junit-reporter --reporter-options mochaFile=testReport/bugs-$(date +%y%m%d%H%M).xml
 
 #./node_modules/mocha/bin/mocha testApp.js --type http --testsuite test_cases/precompile.tsv --no-timeouts --reporter mocha-junit-reporter --reporter-options mochaFile=testReport/precompile-$(date +%y%m%d%H%M).xml
 
