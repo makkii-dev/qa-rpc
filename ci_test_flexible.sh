1dde7707db0892e335580e0e43cc1d8e7f252527
MiaoShi09 <miao@aion.network>
2019-02-27 18:53:17 +0800

    update testcases and test scripts

---
+++ ci_test_flexible.sh
@@ -0,0 +1,21 @@
+
+#！/bin/bash
+## ./ci_test_flexible.sh "${file[@]}" "${type[@]}" false
+
+
+if [ "$1" == "-h" ] ; then
+	echo "USAGE: ./ci_test.sh <list of test data file names> <list of rpc types> <whether starts miner>"
+
+else 
+	if [ "$3" == true ] ; then
+		nohup /run/aionminer -l 127.0.0.1:8008 -t 1 &
+	fi
+
+	for f in "$1"; do
+		for t in "$2"; do 
+			echo $f
+			echo $t
+			./node_modules/mocha/bin/mocha testApp.js --type "$t" --testsuite test_cases/"$f".tsv --no-timeouts --reporter mocha-junit-reporter --reporter-options mochaFile=testReport/"$f"-"$t"-$(date +%y%m%d%H%M).xml
+		done
+	done
+fi
