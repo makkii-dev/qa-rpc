954244dc117ccbb316a5ebcaa46c615f10816116
MiaoShi09 <miao@aion.network>
2019-02-28 15:39:43 +0800

    update flexible testing script

--- ci_test_flexible.sh
+++ ci_test_flexible.sh
@@ -11,10 +11,15 @@ else
 		nohup /run/aionminer -l 127.0.0.1:8008 -t 2 &
 	fi
 
-	for f in "$1"; do
-		for t in "$2"; do 
-			echo $f
-			echo $t
+	echo "$1"
+#	files=(${1//,/})
+	IFS=',' read -r -a files <<< "$1"
+	IFS=',' read -r -a types <<< "$2"
+	for f in "${files[@]}"; do
+		echo "$f"
+		for t in "${types[@]}"; do 
+			
+			echo "$t"
 			./node_modules/mocha/bin/mocha testApp.js --type "$t" --testsuite test_cases/"$f".tsv --no-timeouts --reporter mocha-junit-reporter --reporter-options mochaFile=testReport/"$f"-"$t"-$(date +%y%m%d%H%M).xml
 		done
 	done
